b36f1f94c4aea7298f9b012de35aaa22b8c5981e
Thomas Owens <to57@duke.edu>
2020-02-18 16:24:57 -0500

    Added frontend external API

---
+++ src/slogo/frontend/ITurtle.java
@@ -0,0 +1,102 @@
+package slogo.frontend;
+
+import slogo.utility.Location;
+
+public interface ITurtle {
+
+  /**
+   * Moves the turtle forward in the direction of its heading by a given distance.
+   *
+   * This is 'relative movement', in that the turtle's final location is given relative to its
+   * current location. Upon calling this method, the turtle will move and update the Canvas
+   * accordingly.
+   * @param distance the distance to move forward, or backwards if negative
+   */
+  void moveForward(int distance);
+
+  /**
+   * Moves the turtle to an absolute location.
+   *
+   * This is 'absolute movement', since the turtle's final location does not depend on its current
+   * location. The turtle jumps to the given location without changing heading, and updates the
+   * Canvas with the corresponding line if needed.
+   * @param l the Location to move to
+   */
+  void moveTo(Location l);
+
+  /**
+   * Rotates the turtle's heading by a given angle.
+   *
+   * This is 'relative rotation', which adjusts the turtle's heading by a given offset from its
+   * current heading. Positive angles indicate turning right, or clockwise; negative angles turn
+   * left, or counter-clockwise. The turtle will visually change heading upon update.
+   * @param angle the rotation from the current heading
+   */
+  void rotate(int angle);
+
+  /**
+   * Sets the turtle's heading to a given angle.
+   *
+   * This is 'absolute rotation', which sets the turtle's heading to a given angle. Angles are
+   * measured from the positive x axis, moving counter-clockwise.
+   * @param angle the new heading
+   */
+  void setHeading(int angle);
+
+  /**
+   * Picks up the turtle's 'pen', so that movement is no longer marked on the canvas.
+   *
+   * If the pen is already up, there is no change.
+   */
+  void penUp();
+
+  /**
+   * Puts down the turtle's 'pen', so that movement is marked on the canvas.
+   *
+   * If the pen is already down, there is no change.
+   */
+  void penDown();
+
+  /**
+   * Makes the turtle visible on the canvas.
+   */
+  void show();
+
+  /**
+   * Hides the turtle on the canvas.
+   */
+  void hide();
+
+  /**
+   * Clears all previous paths left by the turtle, leaving a blank canvas.
+   */
+  void clear();
+
+  /**
+   * Gets the current location of the turtle.
+   *
+   * @return the turtle's Location
+   */
+  Location getLocation();
+
+  /**
+   * Gets the current heading of the turtle.
+   *
+   * @return an int, the current heading
+   */
+  int getHeading();
+
+  /**
+   * Gets the pen's current position.
+   *
+   * @return true if the pen is down, false if up
+   */
+  boolean getPenDown();
+
+  /**
+   * Gets the turtle's visibility.
+   *
+   * @return true if the turtle is visible, false if not
+   */
+  boolean getShowing();
+}
---
+++ src/slogo/utility/Location.java
@@ -0,0 +1,5 @@
+package slogo.utility;
+
+public class Location {
+
+}

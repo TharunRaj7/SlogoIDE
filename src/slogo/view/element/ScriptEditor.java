3dd9a0efd4585829310b5b8fd8baa7362ffa0b00
Thomas Owens <to57@duke.edu>
2020-02-24 19:57:43 -0500

    Added Open/Save/Save As buttons

--- src/slogo/view/element/ScriptEditor.java
+++ src/slogo/view/element/ScriptEditor.java
@@ -1,8 +1,20 @@
 package slogo.view.element;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.Property;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import slogo.model.Parser;
 import slogo.view.utility.ButtonFactory;
 
@@ -13,6 +25,9 @@ public class ScriptEditor extends GuiElement {
   private Parser myParser;
   private TextArea input;
   private HBox myButtons;
+  private Button mySaveButton;
+
+  private String myFilePath;
 
   public ScriptEditor(Parser parser) {
     myParser = parser;
@@ -28,17 +43,18 @@ public class ScriptEditor extends GuiElement {
   private void initializeButtons() {
     myButtons = new HBox();
     myButtons.setSpacing(GAP);
+    addButton("Run", e -> myParser.parse(input.getText()));
+    addButton("Open", e -> openFile());
+    mySaveButton = ButtonFactory.button("Save", e -> saveFile(myFilePath));
+    mySaveButton.setDisable(true);
+    myButtons.getChildren().add(mySaveButton);
+    addButton("Save As", e -> saveAsFile());
+  }
 
-    Button runButton = ButtonFactory.button(
-        "Run", e -> myParser.parse(input.getText())
-    );
-
-    Button openButton = ButtonFactory.button(
-        "Open", e -> System.out.println("open")
-    );
-
-    myButtons.getChildren().add(runButton);
-    myButtons.getChildren().add(openButton);
+  private void addButton(String nameProperty, EventHandler<ActionEvent> eh) {
+    if (myButtons != null) {
+      myButtons.getChildren().add(ButtonFactory.button(nameProperty, eh));
+    }
   }
 
   private void initializeLayout() {
@@ -48,6 +64,61 @@ public class ScriptEditor extends GuiElement {
     this.setMinWidth(MIN_WIDTH);
 
     this.add(myButtons, 0, 0);
+  }
+
+  private void openFile() {
+    FileChooser fc = new FileChooser();
+    String dataPath = System.getProperty("user.dir") + "/data";
+    File workingDirectory = new File(dataPath);
+    fc.setInitialDirectory(workingDirectory);
+
+    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Logo files (*.logo)",
+        "*.logo");
+    fc.getExtensionFilters().add(extFilter);
+
+    File file = fc.showOpenDialog(new Stage());
+    try {
+      Scanner scan = new Scanner(file);
+      input.clear();
+      while (scan.hasNextLine()) {
+        input.appendText(scan.nextLine() + "\n");
+      }
+      scan.close();
+      input.requestFocus();
+      myFilePath = file.getAbsolutePath();
+      mySaveButton.setDisable(false);
+    } catch (FileNotFoundException ignored) {}
+  }
+
+  private void saveFile(String fileName) {
+    try {
+      File file = new File(fileName);
+      FileWriter fw = new FileWriter(file);
+
+      fw.write(input.getText());
+      fw.close();
+    } catch (IOException e) {
+      // TODO: REMOVE THIS
+      e.printStackTrace();
+    }
+  }
+
+  private void saveAsFile() {
+    FileChooser fc = new FileChooser();
+    String dataPath = System.getProperty("user.dir") + "/data";
+    File workingDirectory = new File(dataPath);
+    fc.setInitialDirectory(workingDirectory);
+
+    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Logo files (*.logo)",
+        "*.logo");
+    fc.getExtensionFilters().add(extFilter);
+
+    File file = fc.showSaveDialog(new Stage());
+
+    if (file != null) {
+      saveFile(file.getAbsolutePath());
+      mySaveButton.setDisable(false);
+    }
   }
 
 }

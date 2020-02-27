b823b358dda67303501899bc1ae6b33c879670a2
Andrew Krier <ak513@duke.edu>
2020-02-27 00:54:31 -0500

    implemented if and ifelse, fixed settowards with new turtle support

---
+++ src/slogo/commands/If.java
@@ -0,0 +1,48 @@
+package slogo.commands;
+
+import slogo.controller.Turtle;
+
+import java.util.ArrayList;
+
+public class If implements ICommand {
+
+    private Turtle myTurtle;
+    int myArgs = 2;
+    double retVal = 0.0;
+    private ArrayList<ICommand> arguments = new ArrayList<>();
+
+    public If (Turtle turtle) {
+        myTurtle = turtle;
+    }
+
+    @Override
+    public boolean enoughArgs() {
+        return checkArgs() == myArgs;
+    }
+
+    @Override
+    public void setArgument(ICommand command) {
+        arguments.add(command);
+    }
+
+    @Override
+    public void execute() {
+        arguments.get(0).execute();
+        if(arguments.get(0).returnVal() != 0.0) {
+            arguments.get(1).execute();
+            retVal = 1.0;
+        }
+    }
+
+    @Override
+    public double returnVal() {
+        return retVal;
+    }
+
+    @Override
+    public void clearArgs() {
+
+    }
+
+    private int checkArgs() { return arguments.size(); }
+}

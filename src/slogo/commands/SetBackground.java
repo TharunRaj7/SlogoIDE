f1391918b18175d70bfd26574cf9692ee31d2e75
Andrew Krier <ak513@duke.edu>
2020-03-07 01:14:43 -0500

    staging to pull from master

--- src/slogo/commands/SetBackground.java
+++ src/slogo/commands/SetBackground.java
@@ -1,4 +1,39 @@
 package slogo.commands;
 
-public class SetBackground {
+import slogo.controller.TurtleController;
+
+import java.util.ArrayList;
+
+public class SetBackground implements ICommand {
+
+    private TurtleController myTurtle;
+    private int myArgs = 1;
+    private ArrayList<ICommand> arguments = new ArrayList<>();
+
+    public SetBackground (TurtleController turtle) {
+        myTurtle = turtle;
+    }
+
+    @Override
+    public boolean enoughArgs() { return arguments.size() == myArgs; }
+
+    @Override
+    public void setArgument(ICommand command) {
+        arguments.add(command);
+    }
+
+    @Override
+    public void execute() {
+        
+    }
+
+    @Override
+    public double returnVal() {
+        return myTurtle.getLastId();
+    }
+
+    @Override
+    public void clearArgs() {
+        // Should be empty
+    }
 }

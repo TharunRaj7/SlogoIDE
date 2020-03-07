f16878a1e86db819017d7dbc28af203b6dc72667
Tharun Raj <tm272@duke.edu>
2020-03-07 02:04:47 -0500

    Done with display functions

--- src/slogo/controller/Turtle.java
+++ src/slogo/controller/Turtle.java
@@ -25,6 +25,8 @@ public class Turtle implements ITurtle {
     private boolean show;
     private double penSize;
     private Color penColor;
+    private int colorIndex;
+    private int shapeIndex;
     private ImageView image;
 
     public Turtle(int id, Location location, double orientationAngle, String imageFilePath) {
@@ -286,6 +288,18 @@ public class Turtle implements ITurtle {
     }
     public void setPenSize (double penSize) {
         this.penSize = penSize;
+    }
+    public int getColorIndex (){
+        return this.colorIndex;
+    }
+    public int getShapeIndex(){
+        return this.colorIndex;
+    }
+    public void setColorIndex (int index){
+        this.colorIndex = index;
+    }
+    public void setShapeIndex (int index){
+        this.shapeIndex= index;
     }
 
 //    //testing
